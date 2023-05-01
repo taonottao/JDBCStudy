b13d69b2967e2d80424ea899437114ddff64fa77
WangTao <1609832760@qq.com>
2023-05-01 19:00:54 +0800

    jdbc2023学习新

--- JDBC2023/src/JDBCInsert.java
+++ JDBC2023/src/JDBCInsert.java
@@ -4,6 +4,8 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 /**
  * @version 1.0
@@ -12,6 +14,7 @@ import java.sql.SQLException;
  */
 public class JDBCInsert {
     public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
         //JDBC 需要通过以下步骤来完成开发
         //1. 创建并初始化一个数据源
         DataSource dataSource = new MysqlDataSource();
@@ -20,13 +23,21 @@ public class JDBCInsert {
         ((MysqlDataSource)dataSource).setPassword("wangtao");
         //2. 和数据库服务器建立连接
         Connection connection = dataSource.getConnection();
-        //3. 构造 SQL 语句
-        String sql =  "insert  into student2 values(1, '张三')";
+        //3.从控制台读取用户的输入内容
+        System.out.println("请输入学生姓名:");
+        String name = scanner.next();
+        System.out.println("请输入学号:");
+        int id = scanner.nextInt();
+        //4. 构造 SQL 语句
+        String sql =  "insert  into student2 values(?, ?)";
         PreparedStatement statement = connection.prepareStatement(sql);
-        //4. 执行 SQL 语句
-        int ret = statement.executeUpdate(); 
+        statement.setInt(1, id);
+        statement.setString(2, name);
+        System.out.println(statement);
+        //5. 执行 SQL 语句
+        int ret = statement.executeUpdate();
         System.out.println("ret = " + ret);
-        //5. 释放必要的资源
+        //6. 释放必要的资源
         statement.close();
         connection.close();
     }
